59df06592b3628368b08e10349201ebbdad416f3
Taylor Cecil <taylor.cecil22@yahoo.com>
2020-01-03 15:13:03 -0700

    first commit

---
+++ Exercises/groceryStore.js
@@ -0,0 +1,28 @@
+/*var shopper = {
+    groceryList: "Taylor's Shopping",
+    groceryCart: ["kale", "avocado", "tamales"],
+    quantityOfItems: 2,
+    isHealthy: true,
+    preferredStore: function(){                           
+        console.log("I prefer to grow my own food!");     
+    }                                                    
+}
+
+shopper.preferredStore()*/
+
+
+
+
+
+// javascript files are read from top to bottom
+// so after you define your function in your javascript file 
+// you need to call your function below the declaration
+
+var gater = {
+    name: 'Gaitlan',
+    bestFriend: 'Taylor',
+    age: 25, 
+    hobbies: ['building', 'learning', 'growing', 'gardening']
+}
+
+console.log('This is ' + gater.name + ', his best friend is ' + gater.bestFriend + '. Gaitlan is ' + gater.age + ' years old, and enjoys ' + gater.hobbies[0] + gater.hobbies[1] + gater.hobbies[2] + gater.hobbies[3]) 
