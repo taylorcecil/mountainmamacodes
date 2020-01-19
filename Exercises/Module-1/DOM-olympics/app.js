aec9370a61f5609e32ef58228f0c0876a72e393d
Taylor Cecil <taylor.cecil22@yahoo.com>
2020-01-18 17:24:22 -0700

    DOM-olympics done

--- Exercises/Module-1/DOM-olympics/app.js
+++ Exercises/Module-1/DOM-olympics/app.js
@@ -1,15 +1,45 @@
-const h1 = document.createElement('h1');
+const h1 = document.createElement('h1')
 
-h1.textContent = 'JavaScript made this! Woohoo.';
+h1.textContent = 'JavaScript made this! Woohoo.'
 
-document.getElementById('header').appendChild(h1);
+document.getElementById('header').appendChild(h1)
 
-h1.className = 'header';
+h1.className = 'header'
 
-subTitle.innerHTML = '<span class='name'> Taylor </span> wrote this JavaScript';
+const subTitle = document.createElement('subTitle')
 
-subTitle.className = 'header';
+subTitle.innerHTML = '<span class="name">Taylor</span> wrote the JavsScript'
 
-document.getElementById('header').appendChild(subTitle);
+subTitle.className = 'header'
+
+document.getElementById('header').appendChild(subTitle)
+
+const message = document.getElementsByClassName('message')
+const messages = document.getElementById('messages')
+
+message[0].textContent = "Hey! How's it going?"
+message[1].textContent = "It's going well, how about you?"
+message[2].textContent = "Well, well. Thank you."
+message[3].textContent = "Great to hear!"
+
+const clear = document.getElementById('clear-button').addEventListener('click', clearConvo)
+
+// function clearConvo(){
+//     message[0].textContent = ""
+//     message[1].textContent = ""
+//     message[2].textContent = ""
+//     message[3].textContent = ""
+// }
+
+function clearConvo() {
+    for(var i=0; i<message.length; i++){
+        message[i].textContent = ""
+      
+        message[i].style.backgroundColor = "white" 
+    }
+}
+
+
+
 
 
