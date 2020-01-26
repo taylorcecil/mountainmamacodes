19a1b8b9dde6859ddbd4a868f8ab3c5878de3228
Taylor Cecil <taylor.cecil22@yahoo.com>
2020-01-26 14:09:30 -0700

    progress on RPG

--- Exercises/Module-1/colossal-rpg/app.js
+++ Exercises/Module-1/colossal-rpg/app.js
@@ -1,8 +1,8 @@
 const readline = require("readline-sync");
 
 function nameInput(){
-    const name = "Hello hiker!\nYou are 863 miles from home on the Appalachian Trail. You have come across an entry book to write your name in.\nWhat is your name? " 
-    return readline.question(name)
+    const name = "Hello hiker!\nIt's a beautiful, overcast day and you are 863 miles from home on the Appalachian Trail. You have come across an entry book to write your name in.\nWhat is your name? " 
+    const userName = readline.question(name)
 }
 
 nameInput()
@@ -21,6 +21,23 @@ function hike(){
 
 hike()
 
+const enemies = []
+
+function WildEnemyOptions(kind, pace, color, loot){
+    this.kind = kind
+    this.pace = pace
+    this.color = color 
+    this.loot = loot
+}
+
+var bear = new WildEnemyOptions("Bear", "moves fast", "black", "fur")
+var porcupine = new WildEnemyOptions("Porcupine", "moves slow", "brown", "roasting sticks")
+var copperhead = new WildEnemyOptions("Copperhead Snake", "moves fast", "brown", "fangs")
+
+enemies.push(bear)
+enemies.push(porcupine)
+enemies.push(copperhead)
+
 function wildEnemy(){
     const min = 1
     const max = 5
@@ -28,24 +45,10 @@ function wildEnemy(){
 
     if(random === 1){
         console.log(enemies[Math.floor(Math.random() * enemies.length)])
+        const attackRun = "You have come across a wild enemy! Are you going to attack it, or run? "
+        const userInput = readline.question(attackRun)
     }
 }
-
-const enemies = []
-
-function WildEnemyOptions(kind, pace, color){
-    this.kind = kind
-    this.pace = pace
-    this.color = color
-}
-
-var bear = new WildEnemyOptions("Bear", "moves fast", "black")
-var porcupine = new WildEnemyOptions("Porcupine", "moves slow", "brown")
-var copperhead = new WildEnemyOptions("Copperhead Snake", "moves fast", "brown")
-
-enemies.push(bear)
-enemies.push(porcupine)
-enemies.push(copperhead)
 
 
 
