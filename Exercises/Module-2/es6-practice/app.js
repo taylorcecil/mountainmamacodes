7755145c1793a0f6af5b24d210c6e09352fb9258
Taylor Cecil <taylor.cecil22@yahoo.com>
2020-02-08 13:13:16 -0700

    beginning react

--- Exercises/Module-2/es6-practice/app.js
+++ Exercises/Module-2/es6-practice/app.js
@@ -1,13 +1,59 @@
-const carrots = ["bright orange", "ripe", "rotten"];
+// const carrots = ["bright orange", "ripe", "rotten"];
 
-// function mapVegetables(arr) {
-//     return arr.map(function(carrot) {
-//         return { type: "carrot", name: carrot }
-//     })
-// }
+// // function mapVegetables(arr) {
+// //     return arr.map(function(carrot) {
+// //         return { type: "carrot", name: carrot }
+// //     })
+// // }
 
-const mapVeggies = arr => {
-  return arr.map(carrot => { type: "carrot", name: carrot });
-};
+// const mapVeggies = arr => {
+//   return arr.map(carrot => { type: "carrot", name: carrot });
+// };
 
-console.log(mapVeggies(carrots))
+// console.log(mapVeggies(carrots))
+
+const thePeople = [
+  {
+    name: "Angelina Jolie",
+    age: 80
+  },
+  {
+    name: "Eric Jones",
+    age: 2,
+    pets: [{ name: "nitro" }, { name: "ruby" }]
+  },
+  {
+    name: "Paris Hilton",
+    age: 5
+  },
+  {
+    name: "Kayne West",
+    age: 16
+  },
+  {
+    name: "Bob Ziroll",
+    age: 100
+  }
+];
+
+const [setItem, item] = useState();
+const { name, value } = e.target;
+
+// const [,,paris.name = "hilton"] = thePeople
+// console.log(thePeople)
+
+const [, eric] = thePeople;
+eric.name = "taylor cecil";
+console.log(thePeople);
+
+const [, , , kanye] = thePeople;
+kanye.age = 100;
+console.log(thePeople);
+
+// [] for arrays
+// {} for objects
+
+const vegetables = ["carrit", "tomat", "tomato soup"];
+
+const [firstVegetable] = vegetables;
+console.log(firstVegetable);
---
+++ Exercises/Module-3/react-practice/app.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDom.render(
+  <ul>
+    <li>hello</li>
+    <li>goodbye</li>
+    <li>bonjour</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// .render() takes 2 parameters: (what we want to render, where we want it to go in the html)
+
+//similar way to write the above
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
+
+// ******************************************************** //
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1> My name is Taylor </h1>
+      <p> I like to cook, learn about gardening, and grow herbs </p>
+      <ul>
+        <p> This year I would like to plant: </p>
+        <li>Beets</li>
+        <li>Onions</li>
+        <li>Kale</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
